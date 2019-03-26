e2df6ff8b9971fcac56558c68a7704c609cfbaeb
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2019-03-26 13:02:27 +0600

    adding two way binding value+ adding style gobally and inline style

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import Person from './Person/Person';
+
+//class component
+class App extends Component {
+
+state = {
+  persons :[
+    {name:"salam" ,age: 33},
+    {name: "kalam",age: 34},
+    {name:"Rahim" ,age: 32},
+  ],
+  otherstate :[]
+}
+
+    switchNameHandler = (newName) => {
+        // console.log("clicked");
+        //Don't do this -> this.state.persons[0].name ="shakib";
+        this.setState ({
+          persons: [
+            {name: newName ,age      : 23},
+            {name: "Kalam",age       : 24},
+            {name: "Abdur Rahim" ,age: 22},
+          ]
+        })
+      }
+
+      nameChangeHandler = (event) => {
+        this.setState ({
+          persons: [
+            {name: "salam",age : 23},
+            {name:  event.target.value,age   : 24},
+            {name: "Abdur Rahim" ,age: 22},
+          ]
+        })
+      }
+    
+  render(){
+    // className is used for tageting css for App
+
+    const myStyle = {
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'poniter'
+    };
+    return (
+      <div className="App">
+        <h1>Alhamdulillah</h1>
+        <button style={myStyle} onClick={this.switchNameHandler.bind(this,'Nahid')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age = {this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this,'Shishir')} 
+        />
+        
+        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age}
+        changed = {this.nameChangeHandler}/>
+        <Person name={this.state.persons[2].name} age = {this.state.persons[2].age}/>
+        <Person name="shakib" age = "25"/>
+        <Person name="Rafik" age = "25"
+        >I like Football.</Person>
+
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Person/Person.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import   './Person.css'
+
+//function component or js arrow function in es7
+const person = (props) => {
+    return (
+<div className="Person">
+    <p onClick ={props.click}>
+        I am {props.name} and I am {props.age}. Meeting after {Math.floor(Math.random()*30)} years
+    </p>
+    <p>{props.children}</p>
+
+    <input type="text" onChange={props.changed} value={props.name}/>
+
+
+</div>
+    
+        )
+}
+
+export default person;
+
+// class based component -> change state -> Update dom
+// function based component -> change props -> Update dom
+//for class based components -> "this.props"
+//for function based based components -> "props.props_name"
